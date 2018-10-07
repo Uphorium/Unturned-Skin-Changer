2d56e6b67629308cf20d9cbc04ceafd60d1c7ae8
Josiah <stephenjalston@gmail.com>
2018-10-07 14:01:59 +1000

    Fresh start

---
+++ SkinChanger.cs
@@ -0,0 +1,33 @@
+﻿using Rocket.API.Collections;
+using Rocket.Core.Plugins;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CyTic.SkinChanger
+{
+	public class SkinChanger : RocketPlugin<SkinChangerConfiguration>
+	{
+		public override TranslationList DefaultTranslations
+		{
+			get
+			{
+				return new TranslationList()
+				{
+					{"TwentyFour_max_players_set", "max_players set to {0} from {1}"}
+				};
+			}
+		}
+
+		protected override void Load()
+		{
+			
+		}
+
+		protected override void Unload()
+		{
+
+		}
+	}
+}
---
+++ SkinChangerConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Rocket.API;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CyTic.SkinChanger
+{
+	public class SkinChangerConfiguration : IRocketPluginConfiguration
+	{
+		public void LoadDefaults()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
